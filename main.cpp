dd1fe1e90142ca27ef1e08eaa83f33a50b1e86d0
maxim_home <aidmax1234@gmail.com>
2025-02-26 00:21:42 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,53 +1,16 @@
-#include <Arduino.h>
-
-// структура передаваемых данных
-struct Data {
-  int32_t num1 = 0;
-  int32_t num2 = 0;
-};
-
-// данные
-Data data;
-// указатель на данные
-uint8_t *dataP;
-
 void setup() {
-  // Говорим что пины в режиме ввода
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-
-  // Иницируем подключение по сериал порту
-  Serial.begin(9600);
-  
-  // Получаем адрес в памяти для переменной data
-  dataP = (uint8_t *)&data;
+  Serial.begin(9600); // Настройка скорости передачи
 }
 
 void loop() {
-  data.num1 = analogRead(A0);
+  // Чтение данных с аналоговых пинов A0 и A1
+  int sensor1 = analogRead(A0);
+  int sensor2 = analogRead(A1);
   
-  if((digitalRead(10) != 1)||(digitalRead(11) != 1)){
-    Serial.write(dataP, sizeof(Data));
-  }
-    // Serial.println(analogRead(A0));
-
-  //Wait for a bit to keep serial data from saturating
-  delay(1);
+  // Отправка данных в формате: "значение1,значение2\n"
+  Serial.print(sensor1);
+  Serial.print(",");
+  Serial.println(sensor2); // println добавляет символ новой строки
+  
+  delay(50); // Задержка для стабильности передачи
 }
-
-// void setup() {
-//   // initialize the serial communication:
-//   Serial.begin(9600);
-//   pinMode(10, INPUT); // Setup for leads off detection LO +
-//   pinMode(11, INPUT); // Setup for leads off detection LO -
-
-// }
-
-// void loop() {
-  
-  
-//     // send the value of analog input 0:
-//       Serial.println(analogRead(A0));
-//   //Wait for a bit to keep serial data from saturating
-//   delay(1);
-// }
